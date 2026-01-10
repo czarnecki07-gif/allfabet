767a04aa8d83b91a7af1ee88c1055be4a45de389
czarnecki07-gif <czarnecki07@gmail.com>
2026-01-10 17:47:21 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-white text-black">
-      <h1 className="text-4xl font-bold">Fallback działa 🎯</h1>
+    <main>
+      <h1>Fallback działa 🎯</h1>
     </main>
   );
 }
